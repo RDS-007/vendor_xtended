463c0c4c1f9758092dd1b2e214c84ff4524a6714
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-01-30 19:30:49 +0530

    soong: add camera_needs_client_info_lib_defaults and change needs_camera_boottime to needs_camera_boottime_defaults

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -59,7 +59,8 @@ SOONG_CONFIG_xtendedQcomVars += \
     uses_pre_uplink_features_netmgrd \
     uses_qcom_bsp_legacy \
     uses_qti_camera_device \
-    needs_camera_boottime_timestamp
+    needs_camera_boottime_timestamp_defaults \
+    camera_needs_client_info_lib_defaults
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -85,7 +86,8 @@ SOONG_CONFIG_xtendedQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION
 SOONG_CONFIG_xtendedQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_xtendedQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_xtendedQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
-SOONG_CONFIG_xtendedQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_xtendedQcomVars_needs_camera_boottime_timestamp_defaults := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_xtendedQcomVars_camera_needs_client_info_lib_defaults := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
